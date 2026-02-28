429c4ac567daef9b009e398937acd8cc01d4c15a
frassetu <frassetu@gmail.com>
2026-02-28 15:09:04 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 
-const CACHE_NAME='missaghji-cache-v1';
-const CORE_ASSETS=['index.html','styles.css','app.js','offline.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/maskable-512.png'];
+const CACHE_NAME='missaghji-cache-v2';
+const CORE_ASSETS=[
+  'index.html','styles.css','app.js','offline.html','manifest.webmanifest',
+  'icons/icon-192.png','icons/icon-512.png','icons/maskable-512.png'
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE_ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
