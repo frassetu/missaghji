64b7c8aaefbf43ee5fa43c15bcfcf1196d42f099
frassetu <frassetu@gmail.com>
2026-03-02 16:50:51 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['index.html','main.html','offline.html','style.css','app.js'])))});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match('offline.html')))});
