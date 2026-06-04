fd33569e489e3f501165f24c5a6c9ff8cf719d8d
frassetu <frassetu@gmail.com>
2026-06-04 07:33:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='msgop-v1-3-3';
-const ASSETS=['index.html','main.html','style.css','app.js','manifest.json','offline.html','icon.png'];
+const CACHE='msgop-v1-3-4';
+const ASSETS=['index.html','main.html','style.css','app.js','manifest.json','offline.html','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
 self.addEventListener('fetch',e=>{ e.respondWith( fetch(e.request).catch(()=>caches.match(e.request)).then(r=>r||caches.match('offline.html')) ); });
