f63398f9d85857f2fe7fd3d72ee506d6f54c34f6
frassetu <frassetu@gmail.com>
2026-06-04 08:56:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
+const CACHE_NAME = "missaghji-v1";
+const urlsToCache = [
+  "/missaghji/",
+  "/missaghji/index.html",
+  "/missaghji/style.css",
+  "/missaghji/app.js",
+  "/missaghji/offline.html"
+];
 
-const CACHE='msgop-v1-3-4';
-const ASSETS=['./index.html','./main.html','./style.css','./app.js','./manifest.json','./offline.html','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>r||caches.match(e.request)).catch(()=>caches.match(e.request).then(r=>r||caches.match('offline.html'))));});
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match("/missaghji/offline.html"));
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
